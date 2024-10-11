d7d1458dc645da04eb8e734fb32d96836d632e87
Anastasiia Povolotskaia <anastasiia.povolotskaia@gmail.com>
2024-10-11 15:07:39 +0300

    models and db connection related staff separated

---
+++ src/models.py
@@ -0,0 +1,58 @@
+from typing import List
+from beanie import Document
+from datetime import datetime
+from pydantic import BaseModel, EmailStr
+
+
+# Users Models
+class Friend(BaseModel):
+    friend: str
+    friendship_date: datetime = datetime.now()
+
+
+class User(Document):
+    """User database representation"""
+    name: str
+    username: str
+    email: EmailStr
+    hashed_password: str
+    created_at: datetime = datetime.now()
+    friends: List[Friend] = []
+    is_active: bool = True
+    is_superuser: bool = False
+    is_verified: bool = False
+
+    class Settings:
+        name = "Users"
+
+
+class ReadUser(BaseModel):
+    name: str
+    username: str
+    created_at: datetime
+    email: EmailStr
+    friends: List[Friend]
+
+    class Settings:
+        projection = {"_id": False}
+
+
+# Message Models
+class Message(Document):
+    # id: str = Field(hidden_from_schema=True)
+    text: str
+    created_at: datetime = datetime.now()
+    updated_at: datetime = datetime.now()
+    username: str
+
+    class Settings:
+        name = "Messages"
+
+
+# Requests Models
+class FriendsRequests(Document):
+    request_sender: str
+    request_getter: str
+
+    class Settings:
+        name = "FriendsRequests"
--- src/mongo_db.py
+++ src/mongo_db.py
@@ -1,63 +1,14 @@
-from typing import List
 import motor.motor_asyncio
-from beanie import Document
-from config import MONGO_HOST, MONGO_PORT, MONGO_DB
-from datetime import datetime
 from beanie import init_beanie
-from pydantic import BaseModel, EmailStr
+from fastapi_users.db import BeanieBaseUser
+
+from models import (User, ReadUser, FriendsRequests,
+                    Message, )
+from config import MONGO_HOST, MONGO_PORT, MONGO_DB
+
+
 
 DATABASE_URL = f"mongodb://{MONGO_HOST}:{MONGO_PORT}"
-
-
-class Message(Document):
-    # id: str = Field(hidden_from_schema=True)
-    text: str
-    created_at: datetime = datetime.now()
-    updated_at: datetime = datetime.now()
-    username: str
-
-    class Settings:
-        name = "Messages"
-
-
-class Friend(BaseModel):
-    friend: str
-    friendship_date: datetime = datetime.now()
-
-
-class User(Document):
-    """User database representation"""
-    name: str
-    username: str
-    email: EmailStr
-    hashed_password: str
-    created_at: datetime = datetime.now()
-    friends: List[Friend] = []
-    is_active: bool = True
-    is_superuser: bool = False
-    is_verified: bool = False
-
-    class Settings:
-        name = "Users"
-
-
-class ReadUser(BaseModel):
-    name: str
-    username: str
-    created_at: datetime
-    email: EmailStr
-    friends: List[Friend]
-
-    class Settings:
-        projection = {"_id": False}
-
-
-class FriendsRequests(Document):
-    request_sender: str
-    request_getter: str
-
-    class Settings:
-        name = "FriendsRequests"
 
 
 async def get_user_db():
