6bad930c99e864cf98ac6ddc1be65858015ac514
Anastasiia Povolotskaia <anastasiia.povolotskaia@gmail.com>
2024-10-14 14:52:54 +0300

    updated: deleted unnecessary comments and docs, changed names of functions, added HTTP responses with status codes

--- src/routers/friends.py
+++ src/routers/friends.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import logging
 from operator import attrgetter
 from models import User, ReadUser, FriendsRequests, Friend
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, status, Response
 from beanie.odm.operators.find.logical import Or
 
 
@@ -10,99 +10,118 @@ friends_router = APIRouter(prefix="/friends", tags=["Friends"])
 log = logging.getLogger(__name__)
 
 
+async def check_user_existence(username):
+    user = await User.find_one(User.username == username)
+    return user if user else None
+
+
 # Function to check whether two users are already friends
-async def are_users_friends(request: FriendsRequests):
-    # Find both users: the one sending the request and the one accepting it
-    user_sender = await User.find_one(User.username == request.request_sender)
-    user_getter = await User.find_one(User.username == request.request_getter)
-    if not user_sender or not user_getter:
-        raise HTTPException(status_code=400, detail="Bad request: user not found")
+async def check_friendship(request_sender: str,
+                           request_getter: str):
+    # Find the request_getter in DB
+    user_getter = await check_user_existence(request_getter)
 
     # Check if request_sender is already in request_getter's friends list
-    sender_is_friend = any(friend.friend == request.request_sender for friend in user_getter.friends)
+    sender_is_friend = any(friend.friend == request_sender for friend in user_getter.friends)
 
-    if sender_is_friend:
-        return True  # Users are already friends
-    return False  # Users are not friends
+    return sender_is_friend if sender_is_friend else None
 
 
 # Function to check whether friends request already exists
-async def are_friendsrequest(request: FriendsRequests):
+async def check_friendsrequest(request: FriendsRequests):
     # check if request already exists for both users
-    check_requests = await FriendsRequests.find_one(Or({"request_sender": request.request_sender,
-                                                        "request_getter": request.request_getter},
-                                                       {"request_sender": request.request_getter,
-                                                        "request_getter": request.request_sender}))
-    if check_requests:  # request already exists
-        return True  # Error 409 Conflict: Request already exists
-    return False  # Request was not found
+    check_requests = await FriendsRequests.find_one(Or(
+        {"request_sender": request.request_sender,
+         "request_getter": request.request_getter},
+        {"request_sender": request.request_getter,
+         "request_getter": request.request_sender}))
+
+    return check_requests if check_requests else None
 
 
 # post friend's requests
-@friends_router.post("/requests/{username}", summary="Send friends request")
-async def send_request(request: FriendsRequests):
-    """
-    request is a class of model FriendsRequests has the following fields:
-    - **id**: exclude id from swagger to avoid double key error
-    - **request_sender**: username of a user who sends request, type str
-    - **request_getter**: username of a user who gets request, type str
-    """
+@friends_router.post("/requests/{username}",
+                     status_code=status.HTTP_201_CREATED,
+                     summary="Send friends request")
+async def send_request(request: FriendsRequests,
+                       response: Response):
     try:
+        # check if users exists
+        user_sender = await check_user_existence(request.request_sender)
+        user_getter = await check_user_existence(request.request_getter)
+        if not user_sender or not user_getter:
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return {"message": "User not found"}
+
         # check if users are already friends
-        friendship_check = await are_users_friends(request)
-        if friendship_check:
+        are_users_friends = await check_friendship(request.request_sender,
+                                                   request.request_getter)
+        if are_users_friends:
+            response.status_code = status.HTTP_409_CONFLICT
             return {"message": "Users are already friends"}
 
         # check if there is no friends requests between users
-        request_check = await are_friendsrequest(request)
-        if request_check:
-            return {"message": "Error 409 Conflict: Request already exists."}
+        is_request = await check_friendsrequest(request)
+        if is_request:
+            response.status_code = status.HTTP_409_CONFLICT
+            return {"message": "Request already exists."}
 
         # if above conditions are verified send query to create friendsrequest
-        result = await User.find_one({"username": request.request_getter})
-        if result:
-            await FriendsRequests.insert(request)
-            return {"message": "Friend request was sent successfully."}
+        await FriendsRequests.insert(request)
+        return {"message": "Friend request was sent successfully."}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
 # get all friends requests by username
-@friends_router.get("/requests/{username}", summary="Get friend requests")
-async def get_request(username: str):
-    """
-    - **username**: username of a user
-    """
+@friends_router.get("/requests/{username}",
+                    status_code=status.HTTP_200_OK,
+                    summary="Get friend requests")
+async def get_request(username: str,
+                      response: Response):
     try:
+        # check if users exists
+        user_exists = await check_user_existence(username)
+        if not user_exists:
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return {"message": "User not found"}
+
         # get all request for username as a sender and as a getter
         result = await FriendsRequests.find_many(Or(FriendsRequests.request_getter==username,
                                                     FriendsRequests.request_sender==username)).to_list()
         if result:
             return result
-        else:
-            return {"message": "Error 404: No friends requests were found."}
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"message": "No friends requests were found."}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
 # accept friends request
-@friends_router.patch("/requests", summary="Accept friends request")
-async def accept_request(request: FriendsRequests):
-    """
-    request is a class of model FriendsRequests has the following fields:
-    - **id**: exclude id from swagger to avoid double key error
-    - **request_sender**: username of a user who sends request, type str
-    - **request_getter**: username of a user who gets request, type str
-    """
+@friends_router.patch("/requests",
+                      status_code=status.HTTP_200_OK,
+                      summary="Accept friends request")
+async def accept_request(request: FriendsRequests,
+                         response: Response):
     try:
+        # check if users exists
+        user_sender = await check_user_existence(request.request_sender)
+        user_getter = await check_user_existence(request.request_getter)
+        if not user_sender or not user_getter:
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return {"message": "User not found"}
+
         # check if users are already friends
-        friendship_check = await are_users_friends(request)
+        friendship_check = await check_friendship(request.request_sender,
+                                                  request.request_getter)
         if friendship_check:
+            response.status_code = status.HTTP_409_CONFLICT
             return {"message": "Users are already friends"}
 
         # check if there is no friends requests between users
-        request_check = await are_friendsrequest(request)
+        request_check = await check_friendsrequest(request)
         if not request_check:
+            response.status_code = status.HTTP_404_NOT_FOUND
             return {"message": "Request was not found"}
 
         # Create new Friend objects for both users
@@ -132,34 +151,34 @@ async def accept_request(request: FriendsRequests):
 
 
 # get user's friends
-@friends_router.get("/{username}", summary="Get user's friends")
-async def get_friends(username):
-    """
-    - **username**: username of a user
-    """
+@friends_router.get("/{username}",
+                    status_code=status.HTTP_200_OK,
+                    summary="Get user's friends")
+async def get_friends(username: str,
+                      response: Response):
     try:
-        result = await User.find_one(User.username == username)
+        result = await check_user_existence(username)
         if result:
             friends_list = result.friends
             return sorted(friends_list, key=attrgetter('friendship_date'))
-        else:
-            return {"message": "Error 404: No user found."}
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"message": "No user found."}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
 # get number of user's friends
-@friends_router.get("/numfriends/{username}", summary="Get number of user's friends")
-async def get_numfriends(username):
-    """
-    - **username**: username of a user
-    """
+@friends_router.get("/numfriends/{username}",
+                    status_code=status.HTTP_200_OK,
+                    summary="Get number of user's friends")
+async def get_num_of_friends(username,
+                             response: Response):
     try:
-        result = await User.find_one(User.username == username)
+        result = await check_user_existence(username)
         if result:
             friends_list = result.friends
             return f"User {username} has {len(friends_list)} friends"
-        else:
-            return {"message": "Error 404: No user found."}
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"message": "Error 404: No user found."}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
