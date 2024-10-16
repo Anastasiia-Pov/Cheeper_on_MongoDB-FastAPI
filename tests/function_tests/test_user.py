5ca526cd46526a09871deab2a06d1bc9bbe4695b
Anastasiia Povolotskaia <anastasiia.povolotskaia@gmail.com>
2024-10-16 19:21:37 +0300

    added testing for username

---
+++ tests/function_tests/test_user.py
@@ -0,0 +1,22 @@
+import unittest
+from src.service_for_users import check_username
+
+
+class TestUsername(unittest.TestCase):
+    def test_username_with_no_spaces(self):
+        self.assertFalse(check_username("username"), False)
+
+    def test_username_with_spaces(self):
+        self.assertTrue(check_username("username with spaces"), True)
+
+    def test_username_with_special_symbols(self):
+        self.assertTrue(check_username("username!with@specsym&,"), True)
+
+    def test_username_with_specsym_and_spaces(self):
+        self.assertTrue(check_username("username with @ specsym, "), True)
+
+    def test_username_with_underscore(self):
+        self.assertFalse(check_username("username_with_underscore"), False)
+
+    def test_username_with_numerals(self):
+        self.assertFalse(check_username("username1"), False)
