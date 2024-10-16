c06007ed5363b9a2c6e814c227e27540b0c52f4a
Anastasiia Povolotskaia <anastasiia.povolotskaia@gmail.com>
2024-10-16 19:18:18 +0300

    added config for test database

--- src/config.py
+++ src/config.py
@@ -6,3 +6,7 @@ load_dotenv()
 MONGO_HOST = os.environ.get("MONGO_HOST")
 MONGO_PORT = os.environ.get("MONGO_PORT")
 MONGO_DB = os.environ.get("MONGO_DB")
+
+MONGO_HOST_TEST = os.environ.get("MONGO_HOST_TEST")
+MONGO_PORT_TEST = os.environ.get("MONGO_PORT_TEST")
+MONGO_DB_TEST = os.environ.get("MONGO_DB_TEST")
